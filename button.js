c0ac130e5ed019227760447a13c6107b63dbd669
Aysha Muhammed <m.aishat9890@gmail.com>
2021-12-14 09:06:46 -0800

    first commit

---
+++ button.js
@@ -0,0 +1,14 @@
+
+
+function toggle(){
+    let button = document.getElementById("dropdown")
+  
+
+    if (button.style.display === "none"){
+        button.style.display = "block" 
+     
+    }
+    else{
+        button.style.display = "none";
+    }
+}
